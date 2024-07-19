18e813b99d65710b9ea3c2ec38702b0c0e1d2e93
Ranim Elhasan <ranimelhasan62@gmail.com>
2024-07-19 22:18:38 +0300

    Delete HEDEF ASİSTANI/Sproje directory

--- HEDEF ASİSTANI/Sproje/app/src/main/java/com/example/sproje/Goal.java
+++
@@ -1,9 +0,0 @@
-package com.example.sproje;
-
-public class Goal {
-    private String goalName;
-    private String goalDescription;
-    private String startDate;
-    private String endDate;
-    private int dailyGoal;
-}
